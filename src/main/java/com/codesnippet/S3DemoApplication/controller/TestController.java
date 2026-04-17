82778cbee01cf5e5a9de742ea5beb3f01d2b98a0
sivakrishnais <sivakrishnabellamkonda14@gmail.com>
2026-04-17 22:07:51 +0530

    update done

--- src/main/java/com/codesnippet/S3DemoApplication/controller/TestController.java
+++ src/main/java/com/codesnippet/S3DemoApplication/controller/TestController.java
@@ -3,19 +3,20 @@ package com.codesnippet.S3DemoApplication.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return "Hello World";
+        return "Hello World siva krishna welocome";
     }
 
     @GetMapping("/test1")
-    public List<String> test1() {
+    public List<String> tes1() {
         return List.of("Siva", "Krishna", "Reddy");
+
     }
 
 }
